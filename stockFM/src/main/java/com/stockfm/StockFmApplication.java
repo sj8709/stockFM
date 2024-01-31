5ef89953788af3e2be09c51625746217c19b70e7
sj8709 <top5541@naver.com>
2024-01-31 22:08:20 +0900

    init

---
+++ stockFM/src/main/java/com/stockfm/StockFmApplication.java
@@ -0,0 +1,13 @@
+package com.stockfm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockFmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockFmApplication.class, args);
+	}
+
+}
---
+++ stockFM/src/test/java/com/stockfm/StockFmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stockfm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockFmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
